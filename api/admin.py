0f35c1357e588ecf6984ed1b972597ec2b292b70
Amy Gori <amydgori@gmail.com>
2021-07-18 23:50:15 -0400

    Add models to admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Book, BookRecord, User
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Book)
+admin.site.register(BookRecord)
